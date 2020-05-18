ec307532936c568fc1fd8a8ffae3c36843050c57
PatrickOerum@icloud.com <patrickoerum@icloud.com>
2020-05-18 21:15:08 +0200

    Added name to highscore

--- app/controllers/quizController.js
+++ app/controllers/quizController.js
@@ -18,11 +18,12 @@ exports.get_all_highscores = function (req, res) {
 // Add score to highscores
 exports.add_score_to_highscores = function (req, res) {
   // sql query
-  const query = "INSERT INTO high_score(score) VALUES(?)";
+  const query = "INSERT INTO high_score(score, name) VALUES(?)";
 
   var score = req.body.score;
+  var name = req.body.name;
 
-  sql.query(query, [score], (err, rows, fields) => {
+  sql.query(query, [score, name], (err, rows, fields) => {
     if (err) {
       console.log("Error: " + err);
       res.sendStatus(500);
